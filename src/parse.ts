b054c854bb46d8a0f08960089326d049738818fa
Nate Moore <nate@skypack.dev>
2022-02-24 16:22:53 -0600

    feat: use new Astro compiler

--- src/parse.ts
+++ src/parse.ts
@@ -1,10 +1,14 @@
-// import { parse as parseAstro } from '@astrojs/parser';
 import makeSynchronous from 'make-synchronous';
 
-const parseSync = makeSynchronous(async (source) => {
-  const { parse } = await import('@astrojs/compiler');
-  const result = await parse(source);
-  return result;
+const parseSync = makeSynchronous(async (source: string) => {
+  const dynamicImport = new Function('file', 'return import(file)');
+  const { parse } = await dynamicImport('@astrojs/compiler');
+  try {
+    const { ast } = await parse(source);
+    return ast;
+  } catch (e) {
+    console.error(e);
+  }
 });
 
 const parse = (text: string) => parseSync(text);
