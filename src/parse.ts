5d562cbc21257e253443608210e8d41fef10f98d
Antony Faris <acp325@gmail.com>
2022-02-24 16:22:13 -0500

    add new compiler

--- src/parse.ts
+++ src/parse.ts
@@ -1,5 +1,12 @@
-import { parse as parseAstro } from '@astrojs/parser';
+// import { parse as parseAstro } from '@astrojs/parser';
+import makeSynchronous from 'make-synchronous';
 
-const parse = (text: string) => parseAstro(text);
+const parseSync = makeSynchronous(async (source) => {
+  const { parse } = await import('@astrojs/compiler');
+  const result = await parse(source);
+  return result;
+});
+
+const parse = (text: string) => parseSync(text);
 
 export default parse;
