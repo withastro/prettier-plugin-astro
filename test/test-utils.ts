53dd1701739343287f5a4571ddf3ab4c36dc18b2
Antony Faris <acp325@gmail.com>
2022-04-13 12:37:05 -0500

    Refactor test-utils

--- test/test-utils.ts
+++ test/test-utils.ts
@@ -2,11 +2,6 @@ import prettier from 'prettier';
 import { fileURLToPath } from 'url';
 import { expect, it } from 'vitest';
 
-// workaround for vitest to watch for the files
-const srcFiles = import.meta.glob('/src/*.ts', {
-  assert: { type: 'raw' },
-});
-
 /**
  * format the contents of an astro file
  */
@@ -52,73 +47,55 @@ function markdownFormat(
 /**
  * Utility to get `[input, output]` files
  */
-function getFiles(file: any, str: string, md?: string) {
-  const ext = md === 'markdown' ? 'md' : 'astro';
-  let input: string = file[`/test/fixtures/${str}/input.${ext}`];
-  let output: string = file[`/test/fixtures/${str}/output.${ext}`];
+function getFiles(file: any, path: string, isMarkdown = false) {
+  const ext = isMarkdown ? 'md' : 'astro';
+  let input: string = file[`/test/fixtures/${path}/input.${ext}`];
+  let output: string = file[`/test/fixtures/${path}/output.${ext}`];
   // workaround: normalize end of lines to pass windows ci
   if (input) input = input.replace(/(\r\n|\r)/gm, '\n');
   if (output) output = output.replace(/(\r\n|\r)/gm, '\n');
   return { input, output };
 }
 
-function getOptions(files: any, folderName: string) {
+function getOptions(files: any, path: string) {
   let opts: object;
   try {
-    opts = JSON.parse(files[`/test/fixtures/${folderName}/options.json`]);
+    opts = JSON.parse(files[`/test/fixtures/${path}/options.json`]);
   } catch (e) {
     opts = {};
   }
   return opts;
 }
 
-type Options = {
-  mode?: 'default' | 'unaltered' | 'markdown';
-};
-
 /**
  * @param {string} name Test name.
  * @param {any} files Files from import.meta.glob.
- * @param {string} folderName Folder name.
- * @param {Options} [{ mode }={ mode: 'default' }]
- *
- * unaltered: input and output should be the same
- *
- * markdown: for markdown files
+ * @param {string} path Fixture path.
+ * @param {boolean} isMarkdown For markdown files
  */
 export function test(
   name: string,
   files: any,
-  folderName: string,
-  { mode }: Options = { mode: 'default' }
+  path: string,
+  isMarkdown = false
 ) {
-  it(name, async () => {
-    const { input, output } = getFiles(files, folderName, mode);
+  it(`${path}\n${name}`, async () => {
+    const { input, output } = getFiles(files, path, isMarkdown);
 
     expect(input, 'Missing input file').to.not.be.undefined;
     expect(output, 'Missing output file').to.not.be.undefined;
 
-    if (mode === 'unaltered') {
-      expect(
-        input,
-        'Unformated file and formated file are not the same'
-      ).to.be.equal(output);
-    } else {
-      expect(
-        input,
-        'Unformated file and formated file are the same'
-      ).to.not.be.equal(output);
-    }
+    const formatFile = isMarkdown ? markdownFormat : format;
 
-    const formatFile = mode === 'markdown' ? markdownFormat : format;
-
-    const opts = getOptions(files, folderName);
+    const opts = getOptions(files, path);
 
     const formatted = formatFile(input, opts);
     expect(formatted, 'Incorrect formating').toBe(output);
 
     // test that our formatting is idempotent
     const formattedTwice = formatFile(formatted, opts);
-    expect(formatted, 'Formatting is not idempotent').toBe(formattedTwice);
+    expect(formatted === formattedTwice, 'Formatting is not idempotent').toBe(
+      true
+    );
   });
 }
