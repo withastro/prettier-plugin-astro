17af6efda1996d86659489b7a94e40d3e36da260
Erika <3019731+Princesseuh@users.noreply.github.com>
2022-10-17 10:02:24 -0300

    Update dependencies, update compiler version (#293)
    * Update dependencies, update compiler version
    
    * Add changeset
    
    * Update deps
    
    * Re-enable disabled test

---
+++ .changeset/pretty-jars-build.md
@@ -0,0 +1,6 @@
+---
+'prettier-plugin-astro': patch
+---
+
+Fix style tags getting moved inside body tags
+Fix fragments with expressions inside being moved to before the expressions in certain cases
--- test/tests/markdown.test.ts
+++ test/tests/markdown.test.ts
@@ -1,5 +1,3 @@
-// This file can't have the `.test.ts` extension otherwise Vitest will try to run it despite being empty which cause an error
-
 import { test } from '../test-utils';
 
 const files = import.meta.glob('/test/fixtures/markdown/*/*', {
@@ -7,10 +5,9 @@ const files = import.meta.glob('/test/fixtures/markdown/*/*', {
 	as: 'raw',
 });
 
-// This test is currently disabled because a bug moves a style tag when it shouldn't
-/* test(
+test(
 	'can format an Astro file containing an Astro file embedded in a codeblock',
 	files,
 	'markdown/embedded-in-markdown',
 	true
-); */
+);
