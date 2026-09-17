694bc177be908ab46afc584aefe2cfb3e4f60c78
Princesseuh <3019731+Princesseuh@users.noreply.github.com>
2026-09-17 03:20:03 +0200

    fix: preserve embedded CSS docs

--- src/printer/embed.ts
+++ src/printer/embed.ts
@@ -89,11 +89,6 @@ function wrapContent(print: PrintFn, content: Doc, isEmpty: boolean): Doc {
 	];
 }
 
-async function renderEmbeddedDoc(content: Doc, options: ParserOptions): Promise<Doc> {
-	const { formatted } = await doc.printer.printDocToString(content, options);
-	return join(hardline, formatted.trimEnd().split(/\r?\n/));
-}
-
 function embedSass(source: string, options: ParserOptions): Doc {
 	const sassOptions: Partial<SassFormatterConfig> = {
 		tabSize: options.tabWidth,
@@ -163,11 +158,13 @@ export function embed(path: AstPath<AstroNode>, options: ParserOptions) {
 		const parser = styleParsers[lang];
 		if (!parser) return printVerbatim(source);
 		return async (textToDoc: TextToDoc, print: PrintFn) => {
-			const cssSource = options.astroCompressHTML === 'jsx' ? source : manualDedent(source).result;
-			const content = await surfacingErrors(textToDoc, cssSource, { ...options, parser });
+			const content = await surfacingErrors(textToDoc, manualDedent(source).result, {
+				...options,
+				parser,
+			});
 			return wrapContent(
 				print,
-				options.astroCompressHTML === 'jsx' ? content : await renderEmbeddedDoc(content, options),
+				group(replaceEndOfLine(content, hardline), { shouldBreak: true }),
 				false,
 			);
 		};
